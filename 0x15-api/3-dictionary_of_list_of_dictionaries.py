b9f1867c42f7ddd179398098e0da2b755821ccb1
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-24 17:45:37 +0000

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Use Rest API at https://jsonplaceholder.typicode.com/ for a given employee ID,
+returns information about his/her TODO list progress.
+- export data in the JSON format
+"""
+
+
+import sys, requests, json
+
+
+if __name__ == "__main__":
+        users = requests.get("https://jsonplaceholder.typicode.com/users")
+        users = users.json()
+        todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+        todos = todos.json()
+
+        todos_dict = {}
+
+        for user in users:
+            task_list = []
+            for task in todos:
+                if task.get('userId') == user.get('id'):
+                    task_dict = {"username": user.get('username'),
+                                 "task": task.get('title'),
+                                 "completed": task.get('completed')}
+                    task_list.append(task_dict)
+            todos_dict[user.get('id')] = task_list
+
+        with open('todo_all_employees.json', mode='w') as file:
+            json.dump(todos_dict, file)
