852d735f1666716f7bb70431aae548473922a213
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-24 16:55:42 +0000

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Use Rest API at https://jsonplaceholder.typicode.com/ for a given employee ID,
+ returns information about his/her TODO list progress.
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        args_id = {'id': sys.argv[1]}
+        users = requests.get("https://jsonplaceholder.typicode.com/users",
+                            params=args_id).json()
+        args_userId = {'userId': sys.argv[1]}
+        todos = requests.get("https://jsonplaceholder.typicode.com/todos",
+                            params=args_userId).json()
+
+        todos_len = 0
+        todos_arr = []
+        for i in todos:
+            if i.get('completed'):
+                todos_arr.append(i)
+                todos_len += 1
+
+        print("Employee {} is done with tasks({}/{}):".format(
+             users[0].get('name'), todos_len, len(todos)))
+
+        for i in todos_arr:
+            print("\t {}".format(i.get('title')))
