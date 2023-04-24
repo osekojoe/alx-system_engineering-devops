fa06d1ab010c9b7cd8f737ea1ea57e7a6479b175
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-24 17:55:17 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,25 +2,29 @@
 """
 Use Rest API at https://jsonplaceholder.typicode.com/ for a given employee ID,
 returns information about his/her TODO list progress.
+- export data in the CSV format
 """
 
 
-import sys, requests, csv
+import csv
+import requests
+import sys
 
 
 def to_csv(users=None, todos=None):
     """export data in the CSV format"""
-    fieldnames = ["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"]
+    fieldname = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
 
     with open(sys.argv[1] + '.csv', 'w') as csvfile:
-        #map dictionary into output rows
-        write = csv.DictWriter(csvfile, fieldnames=fieldnames,
+        '''map dictionary into output rows'''
+        write = csv.DictWriter(csvfile, fieldname=fieldname,
                                quoting=csv.QUOTE_ALL)
         for i in todos:
             write.writerow({"USER_ID": i.get('userId'),
-                           "USERNAME": users[0].get('username'),
-                           "TASK_COMPLETED_STATUS": i.get('completed'),
-                           "TASK_TITLE": i.get('title')})
+                            "USERNAME": users[0].get('username'),
+                            "TASK_COMPLETED_STATUS": i.get('completed'),
+                            "TASK_TITLE": i.get('title')})
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2 and sys.argv[1].isdigit():
