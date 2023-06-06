cf8f90bbdfd02120fcfee0f148f2eeaca444f667
jdev <20902932+osekojoe@users.noreply.github.com>
2023-06-06 11:59:20 +0000

    How many subscribers?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent":'Mozilla/5.0'
+        "User-Agent": 'Mozilla/5.0'
     }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
