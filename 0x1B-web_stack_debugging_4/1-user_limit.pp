19df134c32657cfe85077c483529b308ccfd1bb6
jdev <20902932+osekojoe@users.noreply.github.com>
2023-06-14 11:05:39 +0200

    Reconfigure OS to increase user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Change OS config to login user 'holberton' & open a file without any error
+
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
+}
+
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 50000/" /etc/security/limits.conf',
+}
